c15815d48a85f14e9796d0720d8a9178b7f3a04f
Olivia Lemmelin <olivialemmelin@gmail.com>
2016-11-21 10:52:04 -0700

    Updating the server.js.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var server = express ();
 var port = 3000;
 
 //Launch the server app.
-//function below is called a callback function.
+    //function below is called a callback function.
 server.listen (port, function (error) {
     //Check to see if the server was unable to start up.
         //if (error !==undefined) means if error is not equals to nothing than is equal to something so there will be an error
@@ -19,6 +19,17 @@ server.listen (port, function (error) {
         //No errors found the server is good to go.
         console.log ('- The server has successfully started on port: ' + port);
     }
+});
 
 
+//----------------------------------------------------------------------------------------------
+//Set the url routes that the server can use.
+
+//Home or root route.
+server.get ('/', function (request, response) {
+    //NOTE: The request object contains information about the user's request (ex. their ip address, headers, coodies, and params)
+
+    //NOTE: The response objct is used to send responses back to the user who made the request.
+
+    response.send ('<h1>Hello World!</h1>');
 });
